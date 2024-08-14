a01e03e773b40e7ee8fc14a7fb08e072372ddff5
herbertmoroni <herbertmoroni@hotmail.com>
2024-08-13 20:56:50 -0600

    added angular material and first attempt using it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,18 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatProgressSpinnerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pomodoro';
-
   timeLeft: number = 1500; // 25 minutes in seconds
   interval: any;
   progress: number = 0;
-
-  constructor(private renderer: Renderer2) {}
 
   ngOnInit(): void {
     this.startTimer();
@@ -26,15 +23,10 @@ export class AppComponent {
       if (this.timeLeft > 0) {
         this.timeLeft--;
         this.progress = ((1500 - this.timeLeft) / 1500) * 100;
-        this.updateProgress(this.progress);
       } else {
         clearInterval(this.interval);
       }
     }, 1000);
-  }
-
-  updateProgress(progress: number) {
-    this.renderer.setStyle(document.documentElement, '--progress', `${progress}%`);
   }
 
   get minutes(): string {
