b4ca21a6925998e191d7f731dfee845f70acc05c
herbertmoroni <herbertmoroni@hotmail.com>
2024-08-13 20:43:56 -0600

    first clock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +10,38 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'pomodoro';
+
+  timeLeft: number = 1500; // 25 minutes in seconds
+  interval: any;
+  progress: number = 0;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    this.startTimer();
+  }
+
+  startTimer() {
+    this.interval = setInterval(() => {
+      if (this.timeLeft > 0) {
+        this.timeLeft--;
+        this.progress = ((1500 - this.timeLeft) / 1500) * 100;
+        this.updateProgress(this.progress);
+      } else {
+        clearInterval(this.interval);
+      }
+    }, 1000);
+  }
+
+  updateProgress(progress: number) {
+    this.renderer.setStyle(document.documentElement, '--progress', `${progress}%`);
+  }
+
+  get minutes(): string {
+    return Math.floor(this.timeLeft / 60).toString().padStart(2, '0');
+  }
+
+  get seconds(): string {
+    return (this.timeLeft % 60).toString().padStart(2, '0');
+  }
 }
